135918c816563b94c645b5777106f47585bcf94b
albertuchr <albertuschristianto27@gmail.com>
2024-10-21 16:07:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,63 @@
 $tes = 2;
 
 echo $tes;
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">To-Do List</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="(nama logout)">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <h1 class="text-center">Welcome, <?= htmlspecialchars($_SESSION['username']) ?></h1>
+                <h2 class="text-center mt-4">Your To-Do Lists:</h2>
+
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <?php if ($todos): ?>
+                            <ul class="list-group">
+                                <?php foreach ($todos as $todo): ?>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                        <?= htmlspecialchars($todo['task']) ?>
+                                        <span class="badge badge-<?= $todo['status'] == 'Complete' ? 'success' : 'warning' ?>">
+                                            <?= $todo['status'] ?>
+                                        </span>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        <?php else: ?>
+                            <p class="text-center">You have no tasks. <a href="add_task.php">Add a task</a></p>
+                        <?php endif; ?>
+                    </div>
+                </div>
+
+                <div class="text-center mt-4">
+                    <a href="add_task.php" class="btn btn-primary">Add New Task</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
