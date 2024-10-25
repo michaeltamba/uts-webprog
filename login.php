4b173951186a4fc2ca6a1f28fede3b2559a65b3e
michaeltamba <lisdahwati0@gmail.com>
2024-10-25 13:48:55 +0700

    upd

--- login.php
+++ login.php
@@ -134,7 +134,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <div class="alert alert-danger"><?php echo $error; ?></div>
     <?php endif; ?>
 
-    <form method="POST" action="index.php">
+    <form method="POST" action="login.php">
         <div class="input-group">
             <i class="fas fa-envelope"></i>
             <input type="email" name="email" placeholder="Enter email" required>
